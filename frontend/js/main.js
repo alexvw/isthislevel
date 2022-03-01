8ab9adb6cfd55cc1ba803a5178c83c1560e1eb6f
Alexander Vander Woude <alex.vander.woude@gmail.com>
2022-02-28 20:22:01 -0500

    fixed compatibility with modern iOS and DeviceOrientation permissions

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -18,83 +18,102 @@ var args = {
     screenAdjusted: false // ( If set to true it will return screen adjusted values. )
 };
 
+function requestOrientationPermission(){
+    DeviceOrientationEvent.requestPermission()
+    .then(response => {
+        if (response == 'granted') {
+            window.addEventListener('deviceorientation', (e) => {
+                // do something with e
+                initDevice();
+                $('.request-button').hide();
+            })
+        }
+    })
+    .catch(console.error)
+};
+
 var gn = new GyroNorm();
 
-gn.init(args).then(function() {
-    gn.start(function(data) {
-        // Process:
-        // data.do.alpha	( deviceorientation event alpha value )
-        // data.do.beta		( deviceorientation event beta value )
-        // data.do.gamma	( deviceorientation event gamma value )
-        // data.do.absolute	( deviceorientation event absolute value )
+function initDevice(){
+    gn.init(args).then(function() {
+        gn.start(function(data) {
+            // Process:
+            // data.do.alpha	( deviceorientation event alpha value )
+            // data.do.beta		( deviceorientation event beta value )
+            // data.do.gamma	( deviceorientation event gamma value )
+            // data.do.absolute	( deviceorientation event absolute value )
+    
+            // data.dm.x		( devicemotion event acceleration x value )
+            // data.dm.y		( devicemotion event acceleration y value )
+            // data.dm.z		( devicemotion event acceleration z value )
+    
+            // data.dm.gx		( devicemotion event accelerationIncludingGravity x value )
+            // data.dm.gy		( devicemotion event accelerationIncludingGravity y value )
+            // data.dm.gz		( devicemotion event accelerationIncludingGravity z value )
+    
+            // data.dm.alpha	( devicemotion event rotationRate alpha value )
+            // data.dm.beta		( devicemotion event rotationRate beta value )
+            // data.dm.gamma	( devicemotion event rotationRate gamma value )
+            //$('#do_alpha').html(data.do.alpha);
+    
+            var betaNear = Math.abs(data.do.beta % 90).toFixed(2);
+            var gammaNear = Math.abs(data.do.gamma % 90).toFixed(2);
+    
+            var betaTo90 = 90 - betaNear;
+            var gammaTo90 = 90 - gammaNear;
+    
+            var betaDistance = Math.min(betaNear, betaTo90).toFixed(2);
+            var gammaDistance = Math.min(gammaNear, gammaTo90).toFixed(2);
+    
+            $('#do_beta').html(betaDistance + "°");
+            $('#do_gamma').html(gammaDistance + "°");
+    
+            var closest = Math.min(betaDistance, gammaDistance).toFixed(2);;
+    
+            if (betaDistance == 0 && gammaDistance == 0) {
+                $('#level-indicator').css('background-color', 'pink');
+                $('#text').html("Suspiciously flat");
+                $('#face').html("🤔");
+            } else if (betaDistance < 0.8 && gammaDistance < 0.8) {
+                $('#level-indicator').css('background-color', 'GREEN');
+                $('#text').html("FLAT");
+                $('#face').html("😍");
+            } else if (closest < 1.5) {
+                $('#level-indicator').css('background-color', 'MEDIUMSEAGREEN');
+                $('#text').html("yes");
+                $('#face').html("😄");
+            } else if (closest < 2.5) {
+                $('#level-indicator').css('background-color', 'GOLD');
+                $('#text').html("close");
+                $('#face').html("😯");
+            } else {
+                $('#level-indicator').css('background-color', 'red');
+                $('#text').html("no");
+                $('#face').html("😠");
+            }
+    
+            /*
+            $('#dm_x').html(data.dm.x);
+            $('#dm_y').html(data.dm.y);
+            $('#dm_z').html(data.dm.z);
+    
+            $('#dm_gx').html(data.dm.gx);
+            $('#dm_gy').html(data.dm.gy);
+            $('#dm_gz').html(data.dm.gz);
+    
+            $('#dm_alpha').html(data.dm.alpha);
+            $('#dm_beta').html(data.dm.beta);
+            $('#dm_gamma').html(data.dm.gamma);
+            */
+        });
+    }).catch(function(e) {
+        // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
+        console.log("Device doesnt support motion: " + e);
+    
+        $('#text').html("iPhone? Needs gyroscope permission");
+        $('.request-button').show();
+    });  
+};
+initDevice();
 
-        // data.dm.x		( devicemotion event acceleration x value )
-        // data.dm.y		( devicemotion event acceleration y value )
-        // data.dm.z		( devicemotion event acceleration z value )
 
-        // data.dm.gx		( devicemotion event accelerationIncludingGravity x value )
-        // data.dm.gy		( devicemotion event accelerationIncludingGravity y value )
-        // data.dm.gz		( devicemotion event accelerationIncludingGravity z value )
-
-        // data.dm.alpha	( devicemotion event rotationRate alpha value )
-        // data.dm.beta		( devicemotion event rotationRate beta value )
-        // data.dm.gamma	( devicemotion event rotationRate gamma value )
-        //$('#do_alpha').html(data.do.alpha);
-
-        var betaNear = Math.abs(data.do.beta % 90).toFixed(2);
-        var gammaNear = Math.abs(data.do.gamma % 90).toFixed(2);
-
-        var betaTo90 = 90 - betaNear;
-        var gammaTo90 = 90 - gammaNear;
-
-        var betaDistance = Math.min(betaNear, betaTo90).toFixed(2);
-        var gammaDistance = Math.min(gammaNear, gammaTo90).toFixed(2);
-
-        $('#do_beta').html(betaDistance + "°");
-        $('#do_gamma').html(gammaDistance + "°");
-
-        var closest = Math.min(betaDistance, gammaDistance).toFixed(2);;
-
-        if (betaDistance == 0 && gammaDistance == 0) {
-            $('#level-indicator').css('background-color', 'pink');
-            $('#text').html("Suspiciously flat");
-            $('#face').html("🤔");
-        } else if (betaDistance < 0.8 && gammaDistance < 0.8) {
-            $('#level-indicator').css('background-color', 'GREEN');
-            $('#text').html("FLAT");
-            $('#face').html("😍");
-        } else if (closest < 1.5) {
-            $('#level-indicator').css('background-color', 'MEDIUMSEAGREEN');
-            $('#text').html("yes");
-            $('#face').html("😄");
-        } else if (closest < 2.5) {
-            $('#level-indicator').css('background-color', 'GOLD');
-            $('#text').html("close");
-            $('#face').html("😯");
-        } else {
-            $('#level-indicator').css('background-color', 'red');
-            $('#text').html("no");
-            $('#face').html("😠");
-        }
-
-        /*
-        $('#dm_x').html(data.dm.x);
-        $('#dm_y').html(data.dm.y);
-        $('#dm_z').html(data.dm.z);
-
-        $('#dm_gx').html(data.dm.gx);
-        $('#dm_gy').html(data.dm.gy);
-        $('#dm_gz').html(data.dm.gz);
-
-        $('#dm_alpha').html(data.dm.alpha);
-        $('#dm_beta').html(data.dm.beta);
-        $('#dm_gamma').html(data.dm.gamma);
-        */
-    });
-}).catch(function(e) {
-    // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
-    console.log("Device doesnt support motion: " + e);
-
-    $('#level-indicator').css('background-color', 'grey');
-    $('#text').html("IDK - your device doesnt support motion");
-});
